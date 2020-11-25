d2a67c2a5142b6e115d668b1f835ff6e9192d179
barreto <60680794+barreto2001@users.noreply.github.com>
2020-11-25 15:19:17 -0500

    ModificacionDropndwon

--- intangibles/vista/intangibles.php
+++ intangibles/vista/intangibles.php
@@ -29,7 +29,6 @@ deleteIntangibleLimitDate();
         <link rel="stylesheet" href="../../css/modales.css">
         <link rel="stylesheet" href="../../css/responsive.css">
         <link rel="stylesheet" href="../../css/loading.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <link rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.js"></script>
@@ -46,13 +45,13 @@ deleteIntangibleLimitDate();
         </script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
             crossorigin="anonymous">
-        <link rel="stylesheet" href="../../css/encuestaIntangible.css">
     <link rel="stylesheet" href="../css/index.css">
     <link rel="stylesheet" href="../css/intangible.css">
     
 </head>
 
 <body>
+    
     <div id="header">
         <img src="../../images/banner1.png" alt="banner" class="img_header">
     </div>
@@ -135,8 +134,7 @@ deleteIntangibleLimitDate();
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-    <script src="../../js/combosymodales.js"></script>
+    
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="../../js/jquery.js"></script>
     <script src="../../js/jquery.redirect.js"></script>
